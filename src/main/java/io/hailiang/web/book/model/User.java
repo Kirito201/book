66f00618270ff97abe30024d9995c8aabe8d8bb9
luhailiang <1176239106@qq.com>
2019-03-13 20:51:52 +0800

    用户管理增删改接口实现

--- src/main/java/io/hailiang/web/book/model/User.java
+++ src/main/java/io/hailiang/web/book/model/User.java
@@ -24,25 +24,13 @@ import java.util.Date;
 public class User {
 
     private Integer userId;
-
-//    @NotBlank(message = "用户名不能为空")
     private String userName;
-
-//    @Length(min = 6, max = 12, message = "密码长度需在6-12之间")
     private String userPassword;
-
-//    @NotBlank(message = "邮箱不能为空")
-//    @Email
     private String userEmail;
-
-//    @NotBlank(message = "手机号不能为空")
     private String userPhone;
-
     private Integer userState;  //约定 1：启用状态  0：停用状态   默认启用
-
     @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
     private Date userCreateTime;
-
     @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
     private Date userLastModifyTime;
 
