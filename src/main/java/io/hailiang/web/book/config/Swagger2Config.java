6acf73a74b8197d6b4bbceadd6f1e09dd163e016
luhailiang <1176239106@qq.com>
2019-03-14 21:26:55 +0800

    Swagger2添加token请求头参数

--- src/main/java/io/hailiang/web/book/config/Swagger2Config.java
+++ src/main/java/io/hailiang/web/book/config/Swagger2Config.java
@@ -5,13 +5,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Auther: luhailiang
@@ -31,6 +37,14 @@ public class Swagger2Config {
 
     @Bean
     public Docket createApi() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("token").description("user token")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build(); //header中的token参数非必填，传空也可以
+        pars.add(tokenPar.build());    //根据每个方法名也知道当前方法在设置什么参数
+
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .enable(swaggerEnabled)
@@ -38,7 +52,10 @@ public class Swagger2Config {
                 //为当前包路径
                 .apis(RequestHandlerSelectors.basePackage("io.hailiang.web.book.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(pars);
+
+
     }
 
     //构建 api文档的详细信息函数
